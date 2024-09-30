be41729bc8555125503163a354635c04366bba1e
Paul Cartwright <111748231+flipflopcod3r@users.noreply.github.com>
2024-09-30 14:26:31 +0100

    Revert "Update page.tsx"
    This reverts commit c77aa2e50d063cff5b46c77974e259682e37cdd8.

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
 						className="rounded"
 						height={450}
 						width={450}
-						src="https://files.stripe.com/links/MDB8YWNjdF8xUTJ5NnNRWEtkWmFwUmF0fGZsX3Rlc3RfeVhVcG5qQnpyZ0JEQUlyZjZvbFZmQ29G005kRUdfgt%"
+						src="https://files.stripe.com/links/MDB8YWNjdF8xUTJ5NnNRWEtkWmFwUmF0fGZsX3Rlc3RfeVVSaXh5VDlzZ0hMWmpqc1lnNkZ2QXho00feCYfq6F"
 						style={{
 							objectFit: "cover",
 						}}
