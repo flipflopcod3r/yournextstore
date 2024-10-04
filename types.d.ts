d15c516d1198d18205469df10a33d24d758e7f31
Paul Cartwright <111748231+flipflopcod3r@users.noreply.github.com>
2024-10-04 15:53:22 +0100

    chore: getting build on vercel

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+declare module '@tailwindcss/forms' {
+    import { PluginCreator } from 'tailwindcss/types/config'
+    const plugin: PluginCreator
+    export default plugin
+  }
+  
+  declare module '@tailwindcss/typography' {
+    import { PluginCreator } from 'tailwindcss/types/config'
+    const plugin: PluginCreator
+    export default plugin
+  }
+  
+  declare module 'tailwindcss-animate' {
+    import { PluginCreator } from 'tailwindcss/types/config'
+    const plugin: PluginCreator
+    export default plugin
+  }
